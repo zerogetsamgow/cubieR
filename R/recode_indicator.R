ebd5b5bd697fd8f78ec4554b3963c144d8ebbc78
zerogetsamgow <sam.gow@gmail.com>
2024-04-04 10:35:24 +1100

    Add first function recode_indicator
    A simple function being used in a project

---
+++ R/recode_indicator.R
@@ -0,0 +1,19 @@
+
+#' A simple function for recoding qualitative variables.
+#'
+#' @description Use this function to recode Qualitative Yes/No data to 1/0.
+#' Note this version converts all non-Yes answers to 0
+#'
+#' @param x the indicator variable to be recoded.
+#'
+#'
+#' @returns a numeric value 1 or 0
+#' @export
+#'
+#' @examples
+
+#' indicators = c("Yes","No","Unsure","y","No")
+#' recode_indicator(indicators)
+#'
+#' # To be added# Define function to recode indicators
+recode_indicator = function(x) {if_else(x %in% c("Yes","yes","y","Y"), 1,0)}
